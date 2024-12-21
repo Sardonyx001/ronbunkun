ca7760ca3006a62997871bde4bdbfe98ac835964
jam <41418470+Sardonyx001@users.noreply.github.com>
2024-12-21 18:11:31 +0900

    refact: add healthcheck endpoint

--- server/routes.go
+++ server/routes.go
@@ -3,6 +3,8 @@ package server
 import (
 	"net/http"
 
+	"log"
+
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -10,12 +12,15 @@ import (
 func ConfigureRoutes(server *Server) {
 	server.Echo.Use(middleware.Recover())
 	server.Echo.Pre(middleware.RemoveTrailingSlash())
+	server.Echo.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "[${time_rfc3339}] ${status} ${method} ${path} (${remote_ip}) ${latency_human}\n",
+		Output: server.Echo.Logger.Output(),
+	}))
 
-	api := server.Echo.Group("/api")
-
-	api.GET("/", todo)
+	server.Echo.GET("/health", healthcheck)
 }
 
-func todo(c echo.Context) error {
-	return c.JSON(http.StatusOK, "Path: "+c.Path())
+func healthcheck(c echo.Context) error {
+	log.Print("Healthcheck request received")
+	return c.JSON(http.StatusOK, map[string]string{"status": "RUNNING"})
 }
