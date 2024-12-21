4e21773dc6ecd48c8c7eb4e5c9850bdb37c6b6b2
jam <41418470+Sardonyx001@users.noreply.github.com>
2024-12-21 18:46:45 +0900

    feat: add arxiv client

--- go.mod
+++ go.mod
@@ -9,11 +9,15 @@ require (
 )
 
 require (
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/marvin-hansen/arxiv v0.1.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/orijtech/otils v0.0.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
+	go.opencensus.io v0.24.0 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/crypto v0.31.0 // indirect
 	golang.org/x/net v0.33.0 // indirect
--- server/routes.go
+++ server/routes.go
@@ -1,23 +1,72 @@
 package server
 
 import (
+	"bytes"
+	"fmt"
 	"net/http"
 
 	"log"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"github.com/marvin-hansen/arxiv/v1"
 )
 
 func ConfigureRoutes(server *Server) {
 	server.Echo.Use(middleware.Recover())
-	server.Echo.Pre(middleware.RemoveTrailingSlash())
 	server.Echo.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "[${time_rfc3339}] ${status} ${method} ${path} (${remote_ip}) ${latency_human}\n",
 		Output: server.Echo.Logger.Output(),
 	}))
 
 	server.Echo.GET("/health", healthcheck)
+
+	api := server.Echo.Group("/api")
+
+	api.GET("/generate", generate)
+}
+
+type Article struct {
+	ID    string `json:"id"`
+	Title string `json:"title"`
+	Url   string `json:"url"`
+}
+
+func generate(c echo.Context) error {
+	log.Print("Generate request received")
+	ctx := c.Request().Context()
+	resultChan, cancel, err := arxiv.Search(ctx, &arxiv.Query{
+		Terms:         "deep learning",
+		MaxPageNumber: 5,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var responseBuf bytes.Buffer
+
+	var articles []Article
+
+	for resultPage := range resultChan {
+		if err := resultPage.Err; err != nil {
+			fmt.Fprintf(&responseBuf, "#%d err: %v", resultPage.PageNumber, err)
+			continue
+		}
+
+		fmt.Fprintf(&responseBuf, "#%d\n", resultPage.PageNumber)
+		feed := resultPage.Feed
+		fmt.Fprintf(&responseBuf, "\tTitle: %s\n\tID: %s\n\tAuthor: %#v\n\tUpdated: %#v\n", feed.Title, feed.ID, feed.Author, feed.Updated)
+
+		for i, entry := range feed.Entry {
+			fmt.Fprintf(&responseBuf, "\n\t\tEntry: #%d Title: %s ID: %s\n\t\tSummary: %s\n\t\tContent: %#v\n\t\tUpdated: %#v\n\t\tLinks: %#v\n",
+				i, entry.Title, entry.ID, entry.Summary.Body, entry.Content, entry.Updated, entry.Link,
+			)
+		}
+		if resultPage.PageNumber >= 2 {
+			cancel()
+		}
+	}
+	return c.JSON(http.StatusOK, articles)
 }
 
 func healthcheck(c echo.Context) error {
