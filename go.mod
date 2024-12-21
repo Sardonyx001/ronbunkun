dc3e3c23be3eb17a35e45192bf147ce01d8fecac
Mohamed Firas Aguel <firas.aguel@gmail.com>
2024-12-21 21:34:12 +0900

    add llm integration

--- go.mod
+++ go.mod
@@ -9,12 +9,16 @@ require (
 )
 
 require (
-	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/dlclark/regexp2 v1.10.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/google/uuid v1.6.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/marvin-hansen/arxiv v0.1.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/orijtech/otils v0.0.2 // indirect
+	github.com/pkoukk/tiktoken-go v0.1.6 // indirect
+	github.com/tmc/langchaingo v0.1.12 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	go.opencensus.io v0.24.0 // indirect
--- server/routes.go
+++ server/routes.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"context"
 	"net/http"
 
 	"log"
@@ -8,6 +9,9 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/marvin-hansen/arxiv/v1"
+
+	"github.com/tmc/langchaingo/llms"
+	"github.com/tmc/langchaingo/llms/ollama"
 )
 
 func ConfigureRoutes(server *Server) {
@@ -21,6 +25,7 @@ func ConfigureRoutes(server *Server) {
 
 	api := server.Echo.Group("/api")
 
+	api.GET("/llm", llmHandler)
 	api.GET("/generate", generate)
 }
 
@@ -76,4 +81,46 @@ func generate(c echo.Context) error {
 func healthcheck(c echo.Context) error {
 	log.Print("Healthcheck request received")
 	return c.JSON(http.StatusOK, map[string]string{"status": "RUNNING"})
+}
+
+type LLMRequest struct {
+	Model  string `json:"model"`
+	System string `json:"system"`
+	Prompt string `json:"prompt"`
+}
+
+type LLMResponse struct {
+	Text string `json:"text"`
+}
+
+func llmHandler(c echo.Context) error {
+	const SYSTEM = "you are an ai translator that translates english papers into japanese. Print only the translation"
+	const PROMPT = "Translate the following: "
+	const textToTranslate = "Because Temporal Logic (TL) is typically used for detecting time-based complex patterns over streams in real time, we aimed at taking advantage of TL after reconstructing records in log files. We have selected Apache web server logs as a case study since Web servers are one of the most preferred targets for hackers and other cyber criminals to intrude systems because of their publicity. Web logs are set of timely recorded events occurred between web servers and clients. In general, Web log files keep each record in the form of request and response together in one line. Reconstructing web server activities as streams based on records in web log files gives us the capability of implementing TL based on streaming data. This way it becomes trivial to achieve fast and better forensics investigation because there are state- of-the-art streaming technologies such as StreamBase, Esper, etc. (“EsperTech - Esper,” n.d., “StreamBase | Complex Event Processing, Event Stream Processing, StreamBase Streaming Platform,” n.d.). We preferred Esper platform and Event Processing Language (EPL) as a standard language to define misuse patterns. Esper provides .NET and Java packages that are easy to implement either for a standalone application or enterprise framework along with EPL, a declarative language for dealing with high frequency time- based event data (“EsperTech - Esper,” n.d.). To the best of our knowledge, there is no platform performing post-mortem log analysis using MSFOMTL, EPL, and reconstruction approach. In addition, cyber security professionals and investigators lack a standard format or language to store and share their previous experiences on log analysis. Besides performance advantage and temporal logical capabilities, our approach would base a platform and a library to store, share, and adjust previously identified patterns of misuses for further analysis. Through the paper, we describe previous work in Section 2, along with some background information in Section 3. Section 4 describes misuse patterns informally that could reside and could be detected from web log records. Then, in Section 5, we define formal versions of those patterns using a special case of TL. Section 6 describes EPL queries that are mapped from TL formulae given in the previous section."
+	s, err := llm(SYSTEM, PROMPT+textToTranslate)
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
+	}
+
+	return c.JSON(http.StatusOK, s)
+}
+
+func llm(system string, prompt string) (string, error) {
+	llm, err := ollama.New(ollama.WithModel("schroneko/gemma-2-2b-jpn-it"))
+	if err != nil {
+		return "", err
+	}
+	ctx := context.Background()
+
+	content := []llms.MessageContent{
+		llms.TextParts(llms.ChatMessageTypeSystem, system),
+		llms.TextParts(llms.ChatMessageTypeHuman, prompt),
+	}
+	completion, err := llm.GenerateContent(ctx, content, llms.WithTemperature(0))
+
+	if err != nil {
+		return "", err
+	}
+
+	return completion.Choices[0].Content, err
 }
